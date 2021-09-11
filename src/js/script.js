6365ab73391285f39dc805e4aca4fcce3870152e
Alex <sir.zubareff2011@yandex.ru>
2021-09-12 00:15:55 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/chevron left solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/chevron right solid.svg"></button>'
+      });
+  });
