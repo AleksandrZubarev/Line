98f690001333362f72868c395b8fd4056d3c2f79
Alex <sir.zubareff2011@yandex.ru>
2021-09-19 23:10:21 +0300

    click

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,20 @@ $(document).ready(function(){
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         }) 
     })
-  });
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();    
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__text').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        })
+    })
+ })
